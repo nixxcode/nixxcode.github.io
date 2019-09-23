2b8e16cbf06e78e32261bc7fcd11c28a96bdb38c
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-23 12:26:06 +0100

    Replace Jekyll with static content

---
+++ connect4react/precache-manifest.db7d08175738c2f5c482510565d81077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebe9dd7f59ae70b773217e1c9d32b69",
+    "url": "/connect4react/index.html"
+  },
+  {
+    "revision": "4141ec0b56a03a623913",
+    "url": "/connect4react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c0d6ecae74400c9e4a51",
+    "url": "/connect4react/static/js/2.f7e3afb5.chunk.js"
+  },
+  {
+    "revision": "4141ec0b56a03a623913",
+    "url": "/connect4react/static/js/main.1cee9128.chunk.js"
+  },
+  {
+    "revision": "fb867d6baec5ff7d7f57",
+    "url": "/connect4react/static/js/runtime~main.09127600.js"
+  }
+]);
